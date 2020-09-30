c47ceec0c5f3a60c562f1a52c6b692c91831e7f6
Özkan Öztürk <ozkozturk@yandex.com>
2020-09-30 21:34:35 +0300

    created actions

---
+++ src/redux/actions.ts
@@ -0,0 +1,104 @@
+import { Dispatch } from 'redux';
+import api from './api';
+import {
+  GET_BOOK_PENDING,
+  GET_BOOK_SUCCESS,
+  GET_BOOK_FAILURE,
+  GET_BOOK_FULFILL,
+  ADD_BOOK_PENDING,
+  ADD_BOOK_SUCCESS,
+  ADD_BOOK_FAILURE,
+  ADD_BOOK_FULFILL,
+  DELETE_BOOK_PENDING,
+  DELETE_BOOK_SUCCESS,
+  DELETE_BOOK_FAILURE,
+  DELETE_BOOK_FULFILL,
+  UPDATE_BOOK_PENDING,
+  UPDATE_BOOK_SUCCESS,
+  UPDATE_BOOK_FAILURE,
+  UPDATE_BOOK_FULFILL,
+} from './actionTypes';
+
+export const getItem = () => (dispatch: Dispatch) => {
+  dispatch({
+    type: GET_BOOK_PENDING,
+  });
+  return api.get('/').then((response) => {
+    dispatch({
+      type: GET_BOOK_SUCCESS,
+      payload: response.data
+    });
+  }).catch((error) => {
+    dispatch({
+      type: GET_BOOK_FAILURE,
+      error,
+    });
+  }).finally(() => {
+    dispatch({
+      type: GET_BOOK_FULFILL,
+    });
+  });
+};
+
+export const addBook = (bookName:string, author: string, price: number) => (dispatch: Dispatch) => {
+  dispatch({
+    type: ADD_BOOK_PENDING,
+  });
+  return api.post('/create', { bookName, author, price }).then((response) => {
+    dispatch({
+      type: ADD_BOOK_SUCCESS,
+      payload: response.data,
+    });
+  }).catch((error) => {
+    dispatch({
+      type: ADD_BOOK_FAILURE,
+      error,
+    });
+  }).finally(() => {
+    dispatch({
+      type: ADD_BOOK_FULFILL,
+    });
+  });
+};
+
+export const updateBook = (id: number, bookName: string, author: string, price: number) => (dispatch: Dispatch) => {
+  dispatch({
+    type: UPDATE_BOOK_PENDING,
+  });
+  return api.post('/update', { bookName, author, price}).then((response) => {
+    dispatch({
+      type: UPDATE_BOOK_SUCCESS,
+      payload: response.data,
+    });
+  }).catch((error) => {
+    dispatch({
+      type: UPDATE_BOOK_FAILURE,
+      error,
+    });
+  }).finally(() => {
+    dispatch({
+      type: UPDATE_BOOK_FULFILL,
+    });
+  });
+};
+
+export const deleteBook = (id: number) => (dispatch: Dispatch) => {
+  dispatch({
+    type: DELETE_BOOK_PENDING,
+  });
+  return api.post('/delete', { id }).then((response) => {
+    dispatch({
+      type: DELETE_BOOK_SUCCESS,
+      payload: response.data,
+    });
+  }).catch((error) => {
+    dispatch({
+      type: DELETE_BOOK_FAILURE,
+      error,
+    });
+  }).finally(() => {
+    dispatch({
+      type: DELETE_BOOK_FULFILL,
+    });
+  });
+};
